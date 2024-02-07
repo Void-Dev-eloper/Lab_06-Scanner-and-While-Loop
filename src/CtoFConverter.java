7a432e3abc8b2277be0d2c579007d1c95c33a0cd
ZN-Zz <141598174+ZN-Zz@users.noreply.github.com>
2024-02-07 10:46:08 -0500

    Removed Comments

--- src/CtoFConverter.java
+++ src/CtoFConverter.java
@@ -1,22 +1,12 @@
 import java.util.Scanner;
 public class CtoFConverter {
     public static void main(String[] args) {
-        /*
-        A program that converts an inputted temperature in C and provides the equivalent temperature in F.
-        Hint: Google is your friend! Given C, solve for F.  Again, check for a valid input value
-        and only respond with the F value if you got it, otherwise loop until you do.
-        Testing: 3 conditions: Bad Input, then test for the known freezing and boiling points.
-
-        F = C * (9/5) + 32
-
-         */
         double fahrenheit;
         double celsius;
         boolean finished = false;
         Scanner in = new Scanner(System.in);
 
         do {
-
             System.out.println("Enter the Celsius: ");
             if (in.hasNextDouble()){
                 celsius = in.nextDouble();
@@ -29,9 +19,5 @@ public class CtoFConverter {
                 System.out.println("Enter a Valid Number: (not) " + "'" + trash + "'");
             }
         }while (!finished);
-
-
-
-
     }
 }
