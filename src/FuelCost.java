fd26980feadf16f15e4e2962a4a0693cc6f2f75a
ZN-Zz <141598174+ZN-Zz@users.noreply.github.com>
2024-02-07 15:09:43 -0500

    Commit FuelCost Problem: FuelCost Finished: added in.hasNExtDouble() and a safe track for said program

--- src/FuelCost.java
+++ src/FuelCost.java
@@ -4,21 +4,36 @@ public class FuelCost{
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
 
-        double gallonsOfGas, fuelEfficiency, pricePerGallon, costPer100Miles, distance;
+        double gallonsOfGas = 1, fuelEfficiency = 1, pricePerGallon = 1, costPer100Miles = 1, distance = 1;
 
         do {
             System.out.print("Enter the number of gallons of gas in the tank: ");
-            gallonsOfGas = in.nextDouble();
+            if (in.hasNextDouble()){
+                gallonsOfGas = in.nextDouble();
+            }else {
+                in.nextLine();
+                gallonsOfGas = 0;
+            }
         } while (gallonsOfGas <= 0);
 
         do {
             System.out.print("Enter the fuel efficiency in miles per gallon: ");
-            fuelEfficiency = in.nextDouble();
+            if (in.hasNextDouble()){
+                fuelEfficiency = in.nextDouble();
+            }else {
+                in.nextLine();
+                fuelEfficiency = 0;
+            }
         } while (fuelEfficiency <= 0);
 
         do {
             System.out.print("Enter the price of gas per gallon: ");
-            pricePerGallon = in.nextDouble();
+            if (in.hasNextDouble()){
+                pricePerGallon = in.nextDouble();
+            }else {
+                in.nextLine();
+                pricePerGallon = 0;
+            }
         } while (pricePerGallon <= 0);
 
         costPer100Miles = 100 / fuelEfficiency * pricePerGallon;
@@ -27,6 +42,5 @@ public class FuelCost{
         System.out.println("The cost per 100 miles is: $" + costPer100Miles);
         System.out.println("The car can go approximately " + distance + " miles with the gas in the tank.");
 
-        in.close();
     }
 }
